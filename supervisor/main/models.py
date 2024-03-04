7beb51cd852432be3928b12ca7f8b9bce4ce2ebd
Dev Maurya <155285196+DevMaurya26@users.noreply.github.com>
2024-03-04 09:53:16 +0530

    Initial Upload
    authentication folder i.e auth
    Supervisor folder i.e main

---
+++ supervisor/main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Allocation_File(models.Model):
+    Created_at = models.DateTimeField(auto_now_add=True,)
+    file_name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return str(self.file_name)
